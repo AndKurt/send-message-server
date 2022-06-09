034db192ca44b7a569ddcf6bd22146245334b280
AndKurt <kurtenkov.as@gmail.com>
2022-06-10 01:42:19 +0300

    feat: add socket

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const cors = require('cors');
 const router = express.Router();
+const socket = require('socket.io');
 //const server = require('http').Server(app);
-//const socket = require('socket.io');
+//const io = require('socket.io')(server);
 
 const PORT = process.env.PORT || 5000;
 
@@ -27,13 +28,26 @@ app.use('/api/auth', authRoute);
 app.use('/api/posts', postRoute);
 app.use('/api/users', usersRoute);
 
-app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+const server = app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
 
-//const io = socket(server, {
-//  cors: 'http://localhost:3000',
-//  credentials: true,
-//});
+const io = socket(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    credentials: true,
+  },
+});
 
-//io.on('connection', (socket) => {
-//  console.log('user connected', socket.id);
-//});
+const onlineUser = new Map();
+
+io.on('connection', (socket) => {
+  socket.on('add-user', (name) => {
+    onlineUser.set(name, socket.id);
+  });
+
+  socket.on('send-msg', (data) => {
+    const sendUserSocket = onlineUser.get(data.recepient);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit('msg-recieve', { data });
+    }
+  });
+});
