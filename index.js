aaeacd2b96313cd5e0d9057c8a030e4279511225
AndKurt <kurtenkov.as@gmail.com>
2022-06-09 17:54:47 +0300

    add: get all users

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 5000;
 // import Routes
 const authRoute = require('./router/auth');
 const postRoute = require('./router/posts');
+const usersRoute = require('./router/users');
 
 //Connect to BD
 mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true }, () =>
@@ -24,6 +25,7 @@ app.use(cors());
 
 app.use('/api/auth', authRoute);
 app.use('/api/posts', postRoute);
+app.use('/api/users', usersRoute);
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
 
